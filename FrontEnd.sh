6dcd785886307111555d77e5e1a79d090601ee0f
Sohail A Iqbal <sohailaiqbal2012@gmail.com>
2024-01-03 14:45:15 -0800

    Adding Topics

--- FrontEnd.sh
+++ FrontEnd.sh
@@ -1,8 +1,8 @@
 dnf install nginx -y
-systemctl enable nginx
-systemctl start nginx
+cp expense.conf /etc/nginx/default.d/expense.conf
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
+systemctl enable nginx
 systemctl restart nginx
